bcdb8cb5708984df3607ca948083613a5156571e
wangjohn <wangjohn@mit.edu>
2014-04-22 11:13:06 -0400

    Working on differential evolution settings.

--- differential_evolution.py
+++ differential_evolution.py
@@ -7,8 +7,35 @@ class DifferentialEvolution(object):
         pass
 
 class DifferentialEvolutionSettings(object):
+    DEFAULT_CROSSOVER_PROBABILITY = 0.5
+    DEFAULT_DIFFERENTIAL_WEIGHT = 0.8
+    DEFAULT_POPULATION_SIZE = 10
+
     def __init__(self, options):
         self.options = options
+
+    def crossover_probability(self, step=None):
+        default = self.DEFAULT_CROSSOVER_PROBABILITY
+        return self._resolve_options_attribute("crossover_probability", default)
+
+    def differential_weight(self, step=None):
+        default = self.DEFAULT_DIFFERENTIAL_WEIGHT
+        return self._resolve_options_attribute("differential_weight", default)
+
+    def population_size(self, step=None):
+        default = self.DEFAULT_POPULATION_SIZE
+        return self._resolve_options_attribute("population_size", default)
+
+    def _resolve_options_attribute(self, attribute_name, default):
+        if attribute_name in self.options:
+            attribute = self.options[attribute_name]
+
+            if hasattr(attribute, "__call__"):
+                return attribute()
+            else:
+                return attribute
+
+        return default
 
 
 class DifferentialEvolutionCrossover(object):

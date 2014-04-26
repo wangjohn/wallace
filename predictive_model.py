e1fddf28eab2f9656a79178830cb4c9dab732d93
wangjohn <wangjohn@mit.edu>
2014-04-26 17:05:00 -0400

    Finishing up the parameters set abstraction.

--- predictive_model.py
+++ predictive_model.py
@@ -15,10 +15,13 @@ class PredictiveModel(object):
         self.parameters = parameters
 
 class ParametersSet(object):
-    def __init__(self, parameters, validity_check=None):
+    def __init__(self, parameters, validity_check=None, defaults=None):
         self.parameters = parameters
         self.validity_check = validity_check
-        self.defaults = {}
+        if defaults == None:
+            self.defaults = {}
+        else:
+            self.defaults = defaults
 
     def get(self, parameter_name):
         if parameter_name in self.parameters:
@@ -28,11 +31,14 @@ class ParametersSet(object):
         raise KeyError("Parameter '%s' was not defined" % parameter_name)
 
     def set(self, parameter_name, value):
-        validity_result = self.validity_check.check_validity(parameter_name, value)
-        if validity_result.is_valid:
-            self.parameters[parameter_name] = value
+        if self.validity_check != None:
+            validity_result = self.validity_check.check_validity(parameter_name, value)
+            if validity_result.is_valid:
+                self.parameters[parameter_name] = value
+            else:
+                raise ValueError("Parameter assignment did not pass validity check: %s" % validity_result.message)
         else:
-            raise ValueError("Parameter assignment did not pass validity check: %s" % validity_result.message)
+            self.parameters[parameter_name] = value
 
 class ParametersValidityCheck(object):
     def check_validity(self, parameter_name, value):

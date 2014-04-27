2be6f6f146ca79dd1fe6223e5d036c2ca3d90356
wangjohn <wangjohn@mit.edu>
2014-04-27 14:13:54 +0000

    Documentation for ParameterValuesSet

--- parameters.py
+++ parameters.py
@@ -1,30 +1,65 @@
 import random
 
-class ParametersSet(object):
-    def __init__(self, parameters, validity_check=None, defaults=None):
-        self.parameters = parameters
+class ParameterValuesSet(object):
+    def __init__(self, parameter_values, validity_check=None, default_values=None):
+        self.parameter_values = parameter_values
         self.validity_check = validity_check
-        if defaults == None:
-            self.defaults = {}
+        if default_values == None:
+            self.default_values = {}
         else:
-            self.defaults = defaults
+            self.default_values = default_values
 
     def get(self, parameter_name):
-        if parameter_name in self.parameters:
-            return self.parameters[parameter_name]
-        if parameter_name in self.defaults:
-            return self.defaults[parameter_name]
+        if parameter_name in self.parameter_values:
+            return self.parameter_values[parameter_name]
+        if parameter_name in self.default_values:
+            return self.default_values[parameter_name]
         raise KeyError("Parameter '%s' was not defined" % parameter_name)
 
     def set(self, parameter_name, value):
         if self.validity_check != None:
             validity_result = self.validity_check.check_validity(parameter_name, value)
             if validity_result.is_valid:
-                self.parameters[parameter_name] = value
+                self.parameter_values[parameter_name] = value
             else:
                 raise ValueError("Parameter assignment did not pass validity check: %s" % validity_result.message)
         else:
-            self.parameters[parameter_name] = value
+            self.parameter_values[parameter_name] = value
+
+    @classmethod
+    def create_from_dict(self, dictionary):
+        """
+        Takes a dictionary and converts it into a ParameterValuesSet. Dictionaries
+        should be of the following form:
+
+        dictionary = {
+            "range_parameter_name": {
+                "type": "range",
+                "lower_bound": 0.0,
+                "upper_bound": 1.0,
+                "value": 0.3423, # optional
+                "default": 0.5, # optional
+            },
+            "integer_range_parameter_name": {
+                "type": "integer_range",
+                "lower_bound": 123,
+                "upper_bound": 156,
+                "value": 133, # optional
+                "default": 150, # optional
+            },
+            "category_parameter_name": {
+                "type": "category",
+                "categories": [True, False],
+                "weights": [0.3, 0.6], # optional
+                "value": True, # optional
+                "default": True, # optional
+            }
+        }
+
+        Currently supporting three types of parameters: range, integer_range,
+        and category.
+        """
+
 
 class ParametersValidityCheck(object):
     def check_validity(self, parameter_name, value):
@@ -42,6 +77,9 @@ class ParametersGeneralValidityCheck(ParametersValidityCheck):
 
     def set_range_parameter(self, parameter_name, lower_bound, upper_bound):
         self.parameters[parameter_name] = RangeParameter(lower_bound, upper_bound)
+
+    def set_integer_range_parameter(self, parameter_name, lower_bound, upper_bound):
+        self.parameters[parameter_name] = IntegerRangeParameter(lower_bound, upper_bound)
 
     def set_category_parameter(self, parameter_name, categories, weights=None):
         self.parameters[parameter_name] = CategoryParameter(categories, weights)
@@ -92,6 +130,31 @@ class RangeParameter(BasicParameter):
             message = "Value '%s' is out of parameter range: [%s, %s]" %
                     (value, self.lower_bound, self.upper_bound)
             return ParametersValidityCheckResult(False, message)
+
+class IntegerRangeParameter(BasicParameter):
+    def __init__(self, lower_bound, upper_bound):
+        BasicParameter.__init__(self, "range")
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self._check_initialization()
+
+    def get_valid_value(self, data=None):
+        return random.randrange(lower_bound, upper_bound)
+
+    def check_validity(self, value):
+        if not isinstance(value, int):
+            message = "Value '%s' is not an integer"
+            return ParametersValidityCheckResult(False, message)
+        if self.lower_bound <= value and value <= self.upper_bound:
+            return ParametersValidityCheckResult(True)
+        else:
+            message = "Value '%s' is out of parameter range: [%s, %s]" %
+                    (value, self.lower_bound, self.upper_bound)
+            return ParametersValidityCheckResult(False, message)
+
+    def _check_initialization(self):
+        if not (isinstance(self.lower_bound, int) and isinstance(self.upper_bound, int)):
+            raise ValueError("Lower and upper bounds for IntegerRangeParameter must be integers")
 
 class CategoryParameter(BasicParameter):
     def __init__(self, categories, weights = None):

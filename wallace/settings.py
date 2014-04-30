72bbec09bbc658100a9f4662f6f01c246f82db19
wangjohn <wangjohn@mit.edu>
2014-04-30 04:31:14 +0000

    Revamping settings to be a lot simpler. Basically just a hash now.

--- wallace/settings.py
+++ wallace/settings.py
@@ -1,10 +1,10 @@
 class AbstractSettings(object):
     DEFAULTS = {
-            "fitness_evaluation_crossfold_partitions": 10,
-            "model_tracking_models_to_track": 50,
-            "differential_evolution_crossover_probability": 0.5,
-            "differential_evolution_differential_weight": 0.8,
-            "differential_evolution_population_size": 20
+            "fitness_evaluation.crossfold_partitions": 10,
+            "model_tracking.models_to_track": 50,
+            "differential_evolution.crossover_probability": 0.5,
+            "differential_evolution.differential_weight": 0.8,
+            "differential_evolution.population_size": 20
         }
 
     def __init__(self, settings=None):
@@ -13,27 +13,23 @@ class AbstractSettings(object):
             for attribute_name, value in settings.iteritems():
                 self.set_attribute(attribute_name, value)
 
-    def _resolve_options_attribute(self, attribute_name, *args):
-        if attribute_name in self.settings:
-            attribute = self.settings[attribute_name]
-
-            if hasattr(attribute, "__call__"):
-                return attribute(*args)
-            else:
-                return attribute
-        elif attribute_name in self.DEFAULTS:
-            return self.DEFAULTS[attribute_name]
+    def _resolve_attribute(self, attribute):
+        if hasattr(attribute, "__call__"):
+            return attribute(*args)
         else:
-            raise ValueError("Settings have no attribute '%s'" % attribute_name)
+            return attribute
 
     def set_attribute(self, attribute_name, value):
-        if attribute_name not in self.settings:
-            getter_method = lambda self, *args : self._resolve_options_attribute(attribute_name, *args)
-            setattr(self.__class__, attribute_name, getter_method)
         self.settings[attribute_name] = value
+
+    def get_attribute(self, attribute_name):
+        if attribute_name in self.settings:
+            return self._resolve_attribute(self.settings[attribute_name])
+        elif attribute_name in self.DEFAULTS:
+            return self._resolve_attribute(self.DEFAULTS[attribute_name])
+        else:
+            raise ValueError("Settings have no attribute '%s'" % attribute_name)
 
     @classmethod
     def set_default(klass, attribute_name, value):
         klass.DEFAULTS[attribute_name] = value
-        getter_method = lambda self : self._resolve_options_attribute(attribute_name)
-        setattr(klass, attribute_name, getter_method)

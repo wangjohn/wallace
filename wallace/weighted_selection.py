1e0de1c30d0dd1fcfc3db46ec37c782426bdbc7d
wangjohn <wangjohn@mit.edu>
2014-04-30 16:54:20 -0400

    Adding tapering ability.

--- wallace/weighted_selection.py
+++ wallace/weighted_selection.py
@@ -11,22 +11,19 @@ class WeightedSelection(object):
         self.weighted_selections[selection] = weight
         self.weighted_selections = self.normalize_weights(self.weighted_selections)
 
-    def increase_weight(self, selection, learning_parameter=0.05):
+    def increase_weight(self, selection, learning_parameter=0.05, taper=True):
         if selection not in self.weighted_selections:
             raise ValueError("Selection '%s' is not known by the WeightedSelection." % selection)
 
         selection_weight = self.weighted_selections[selection]
-        new_selection_weight = (1.0 + learning_parameter) * selection_weight
-        attrition_rate = 1 + float(selection_weight - new_selection_weight) / (1 - selection_weight)
+        if taper:
+            tapering_rate = float(selection_weight) / 1.0
+            new_selection_weight = (1.0 + learning_parameter*tapering_rate) * selection_weight
+        else:
+            new_selection_weight = (1.0 + learning_parameter) * selection_weight
 
-        weighted_selections = {}
-        for sel, weight in self.weighted_selections.iteritems():
-            if sel == selection:
-                weighted_selections[sel] = new_selection_weight
-            else:
-                weighted_selections[sel] = weight * attrition_rate
-
-        self.weighted_selections = weighted_selections
+        self.weighted_selections[selection] = new_selection_weight
+        self.weighted_selections = self.normalize_weights(self.weighted_selections)
 
     def normalize_weights(self, weighted_selections):
         non_weighted = 0

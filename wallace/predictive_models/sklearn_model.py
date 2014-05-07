64220f748e0d904c2c077194091d062e6f27d9d0
wangjohn <wangjohn@mit.edu>
2014-05-07 17:21:45 -0400

    Adding preprocessing for categorical variables.

---
+++ wallace/predictive_models/sklearn_model.py
@@ -0,0 +1,31 @@
+from sklearn import preprocessing
+
+class SklearnModel(object):
+    def get_dependent_variable_data(self, dataset):
+        dependent_column = dataset.get_filtered_column(self.dependent_variable)
+        encoder = self.get_encoder(dataset)
+        return encoder.transform(dependent_column).to_array()
+
+    def get_independent_variable_data(self, dataset):
+        filtered_matrix = dataset.get_filtered_matrix(self.independent_variables)
+        encoder = self.get_encoder(dataset)
+        return encoder.fit(filtered_matrix)
+
+    def get_encoder(self, dataset):
+        filtered_data_types = dataset.get_filtered_data_types(self.independent_variables)
+        categorical_features = []
+        for i in xrange(len(filtered_data_types)):
+            data_type = filtered_data_types[i]
+            if data_type == "string" or data_type == "boolean":
+                categorical_features.append(i)
+
+        return preprocessing.OneHotEncoder(categorical_features=categorical_features)
+
+class TrainedSklearnModel(TrainedPredictiveModel):
+    def __init__(self, predictive_model, fitted_regression):
+        TrainedPredictiveModel.__init__(self, predictive_model)
+        self.fitted_regression = fitted_regression
+
+    def predict(self, dataset):
+        independent_data = self.predictive_model.get_independent_variable_data(dataset)
+        return self.fitted_regression.predict(independent_data)

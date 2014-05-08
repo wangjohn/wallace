c7bb412b27dad25a1f7bb07e89891390aa60eec9
wangjohn <wangjohn@mit.edu>
2014-05-08 18:41:18 +0000

    Testing dependent variable data.

--- tests/predictive_models/test_sklearn_model.py
+++ tests/predictive_models/test_sklearn_model.py
@@ -21,10 +21,15 @@ class SklearnModelTest(TestCase):
 
         self.sklearn_model = SklearnModel(self.settings, self.parameter_set, self.dependent_variable, self.independent_variables)
 
-
-    def test_changing_categorical_independent_variables(self):
+    def test_categorical_independent_variables(self):
         independent_variable_data = self.sklearn_model.get_independent_variable_data(self.dataset)
 
         self.assertEqual(5, len(independent_variable_data))
         num_categories = len(independent_variable_data[0])
         self.assertEqual(5, num_categories)
+
+    def test_dependent_variable_data(self):
+        dependent_variable_data = self.sklearn_model.get_dependent_variable_data(self.dataset)
+
+        self.assertEqual(5, len(dependent_variable_data))
+        self.assertListEqual([1,2,3,5,2], dependent_variable_data)

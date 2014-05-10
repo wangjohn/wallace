3c3deaaf0fbdc6d19810a27b216fa3e99a6ec450
wangjohn <wangjohn@mit.edu>
2014-05-10 14:32:21 +0000

    Transforming columns of data.

--- wallace/dataset_transformations/dataset_transformation.py
+++ wallace/dataset_transformations/dataset_transformation.py
@@ -9,7 +9,20 @@ class DatasetTransformation(object):
             variables = [DatasetVariable(i) for i in xrange(dataset.num_cols)]
 
         filtered_matrix = dataset.get_filtered_matrix(variables)
-        raise NotImplementedError()
+        num_cols = len(filtered_matrix[0])
+        transformed_columns = []
+        for j in xrange(num_cols):
+            current_column = []
+            for i in xrange(len(filtered_matrix)):
+                current_column.append(filtered_matrix[i][j])
+
+            current_transformed = self.transform_column(column)
+            transformed_columns.extend(current_transformed)
+
+        return self.rotate_matrix(transformed_columns)
+
+    def rotate_matrix(self, data_matrix):
+        pass
 
     def transform_column(self, column):
         raise NotImplementedError()

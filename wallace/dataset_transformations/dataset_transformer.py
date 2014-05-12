d0f495070f79aa7b736eb2ffb94ea68a7450efbe
wangjohn <wangjohn@mit.edu>
2014-05-12 21:04:15 +0000

    Implementing dataset merge on the dataset transformer.

--- wallace/dataset_transformations/dataset_transformer.py
+++ wallace/dataset_transformations/dataset_transformer.py
@@ -21,7 +21,44 @@ class DatasetTransformer(object):
             current_dataset = transformation.transform(dataset, variables)
             resulting_datasets.append(current_dataset)
 
-        return Dataset.merge_datasets(resulting_datasets)
+        return self.merge_datasets(resulting_datasets)
+
+    def merge_datasets(self, datasets):
+        if len(datasets) < 1:
+            raise ValueError("Must specify at least one dataset to merge.")
+
+        first_dataset = datasets[0]
+        num_rows = first_dataset.num_rows
+        data_matrix = []
+        for row in first_dataset:
+            data_matrix.append(list(row))
+
+        if first_dataset.headers == None:
+            headers = None
+        else:
+            headers = list(first_dataset.headers)
+
+        for dataset in datasets[1:]:
+            headers = self.merge_headers(headers, dataset)
+            data_matrix = self._merge_data_matrix(data_matrix, dataset, num_rows)
+        return Dataset(data_matrix, headers)
+
+    def _merge_headers(self, headers, dataset):
+        if (headers == None and dataset.headers != None) or (headers != None and dataset.headers == None):
+            raise ValueError("Inconsistent headers when merging datasets. Either all datasets must have headers or none can have headers.")
+
+        if headers != None:
+            for header in dataset.headers:
+                headers.append(header)
+            return headers
+
+    def _merge_data_matrix(self, data_matrix, dataset, num_rows):
+        if dataset.num_rows != num_rows:
+            raise ValueError("Merging datasets requires that all datasets have the same number of rows.")
+
+        for i in xrange(num_rows):
+            data_matrix[i].extend(dataset.get_row(i))
+        return data_matrix
 
     def _initialize_transformations(self, transformations):
         if transformations == None:

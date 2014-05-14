69882d6118de48ec3a7f111cbce952f3c789ea38
wangjohn <wangjohn@mit.edu>
2014-05-14 12:04:00 -0400

    Adding descriptions to settings.

--- wallace/settings.py
+++ wallace/settings.py
@@ -6,9 +6,26 @@ from wallace.dataset_transformations.sqrt_transformation import SqrtTransformati
 from wallace.dataset_transformations.box_cox_transformation import BoxCoxTransformation
 
 class AbstractSettings(object):
+    DESCRIPTIONS = {
+            "fitness_evaluation.crossfold_partitions": "Default number of partitions (k) to use when doing k-fold cross validation.",
+            "fitness_evaluation.evaluation_method": "Default method for evaluating the fitness of a model.",
+            "model_tracking.models_to_track": "Specify the number of the best models to return after the run has completed.",
+            "optimization_algorithm_tracking.tracking_log_filename": "The file which all tracking on the optimization algorithm will be written to.",
+            "dataset.maximum_dataset_size": "When reading in a dataset, this is the maximum number of rows allowed.",
+            "dataset.randomize_file_reader": "If a file has more rows than `dataset.maximum_dataset_size` allows, then read in all of rows and randomly choose the rows to keep.",
+            "dataset.remove_rows_with_missing_data": "Remove a row if it has missing data.",
+            "dataset_transformation.transform_datasets": "Perform the default transformations defined in `dataset_transformations.default_transformations` on a dataset when it is being read into memory.",
+            "dataset_transformation.log_transformation_base": "Default base for performing a log transformation.",
+            "dataset_transformation.default_transformations": "Transformations to be applied to a dataset automatically if `dataset_transformation.transform_datasets` is True.",
+            "differential_evolution.crossover_probability": "The probability of mutating a given value in differential evolution (CR).",
+            "differential_evolution.differential_weight": "Used for weighting differences in differential evolution (F).",
+            "optimization_algorithm.population_size": "Default number of models in the population for a given step of an optimization algorithm.",
+            "independent_variable_selection.initial_independent_variables_percentage": "Percentage of total number of variables to start with when initializing an optimization algorithm."
+        }
+
     DEFAULTS = {
             "fitness_evaluation.crossfold_partitions": 10,
-            "fitness_evaluation.evaluation_method": MeanSquaredError,
+            "fitness_evaluation.evaluation_method": MeanSquaredError
             "model_tracking.models_to_track": 50,
             "optimization_algorithm_tracking.tracking_log_filename": "logs/optimization_algorithm_logs.log",
             "dataset.maximum_dataset_size": 50000,
@@ -45,6 +62,12 @@ class AbstractSettings(object):
             return self._resolve_attribute(self.DEFAULTS[attribute_name], *args)
         else:
             raise ValueError("Settings have no attribute '%s'" % attribute_name)
+
+    def get_description(self, attribute_name):
+        if attribute_name in self.DESCRIPTIONS:
+            return self.DESCRIPTIONS[attribute_name]
+        else:
+            raise ValueError("No description exists for attribute '%s'" % attribute_name)
 
     def has(self, attribute_name):
         return (attribute_name in self.settings) or (attribute_name in self.DEFAULTS)

83f832c89b65a550d4ac24039803d407b149430a
wangjohn <wangjohn@mit.edu>
2014-05-16 13:52:16 +0000

    Changing the settings initialization.

--- examples/carvana.py
+++ examples/carvana.py
@@ -8,16 +8,9 @@ if not path in sys.path:
 from wallace.initialization import WallaceInitialization
 
 settings = {
-    "fitness_evaluation.crossfold_partitions": 10,
-    "model_tracking.models_to_track": 50,
-
-    "differential_evolution.crossover_probability": 0.5,
-    "differential_evolution.differential_weight": 0.8,
-
     "independent_variable_selection.initial_independent_variables_percentage": 0.25,
-
     "optimization_algorithm.population_size": 40,
-    "optimization_algorithm.finishing_criteria.max_steps": 2
+    "optimization_algorithm.finishing_criteria.max_steps": 200
     }
 
 dependent_variable = "IsBadBuy"

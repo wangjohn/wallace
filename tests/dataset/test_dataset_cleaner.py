c8df69100933fb80ab760ac1de25c7044c4ec581
wangjohn <wangjohn@mit.edu>
2014-05-17 12:56:59 -0400

    Handling missing data in a better way (sparse columns).

--- tests/dataset/test_dataset_cleaner.py
+++ tests/dataset/test_dataset_cleaner.py
@@ -1,14 +1,15 @@
 from unittest import TestCase
 from datetime import datetime
 
-from wallace.dataset_cleaner import DatasetCleaner, MissingDataException
+from wallace.dataset_cleaner import DatasetCleaner
 from wallace.settings import AbstractSettings
 from wallace.data_type import DataType
 
 class DatasetCleanerTest(TestCase):
     def setUp(self):
         self.settings = AbstractSettings({
-                "dataset.remove_rows_with_missing_data": True
+                "dataset.remove_rows_with_missing_data": True,
+                "dataset.maximum_missing_data_percentage": 1.0
             })
         self.data_matrix = [
                 ["3421", "1232", "hello", "t", "5/12/2003"],
@@ -20,14 +21,9 @@ class DatasetCleanerTest(TestCase):
         self.dataset_cleaner = DatasetCleaner(self.settings, self.data_matrix)
 
     def test_clean_entry_on_missing_data_raises_exception(self):
-        with self.assertRaises(MissingDataException):
-            self.dataset_cleaner.clean_entry("NaN", DataType("string"))
-
-        with self.assertRaises(MissingDataException):
-            self.dataset_cleaner.clean_entry("", DataType("float"))
-
-        with self.assertRaises(MissingDataException):
-            self.dataset_cleaner.clean_entry("NA", DataType("integer"))
+        self.assertEqual(None, self.dataset_cleaner.clean_entry("NaN", DataType("string")))
+        self.assertEqual(None, self.dataset_cleaner.clean_entry("", DataType("float")))
+        self.assertEqual(None, self.dataset_cleaner.clean_entry("NA", DataType("integer")))
 
     def test_clean_entry_on_date(self):
         cleaned_entry = self.dataset_cleaner.clean_entry("4/25/2013", DataType("date"))
@@ -42,9 +38,12 @@ class DatasetCleanerTest(TestCase):
         self.assertEqual(timestamp, cleaned_row[1])
         self.assertEqual("Bob", cleaned_row[2])
 
-    def test_clean_entry_on_row_raises_exception_for_missin_data(self):
-        with self.assertRaises(MissingDataException):
-            cleaned_row = self.dataset_cleaner.clean_row(["NaN", "4/25/2013", "Bob"], [DataType("integer"), DataType("date"), DataType("string")])
+    def test_clean_entry_on_row_raises_exception_for_missing_data(self):
+        cleaned_row = self.dataset_cleaner.clean_row(["NaN", "4/25/2013", "Bob"], [DataType("integer"), DataType("date"), DataType("string")])
+        timestamp = (datetime(2013, 4, 25) - datetime(1970, 1, 1)).total_seconds()
+        self.assertEqual(None, cleaned_row[0])
+        self.assertEqual(timestamp, cleaned_row[1])
+        self.assertEqual("Bob", cleaned_row[2])
 
     def test_fully_cleaning_the_dataset(self):
         cleaned_matrix = self.dataset_cleaner.clean()

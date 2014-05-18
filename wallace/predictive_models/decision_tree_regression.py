1717a4da3422c5cb1d45ece282b5318a6a73269e
wangjohn <wangjohn@mit.edu>
2014-05-18 14:30:34 -0400

    Adding decision trees, extra trees, rf, and gradient boosting.

--- wallace/predictive_models/decision_tree_regression.py
+++ wallace/predictive_models/decision_tree_regression.py
@@ -1,21 +1,12 @@
-from sklearn import linear_model
+from sklearn import tree
 from wallace.predictive_models.sklearn_model import SklearnModel, TrainedSklearnModel
 from wallace.parameters import ParametersGeneralValidityCheck
 
-class LassoRegression(SklearnModel):
+class DecisionTreeRegression(SklearnModel):
     def train(self, dataset):
-        model = linear_model.Lasso(alpha=self.get_alpha())
+        model = tree.DecisionTreeRegressor()
         independent_data = self.get_independent_variable_data(dataset)
         dependent_data = self.get_dependent_variable_data(dataset)
         trained_regression = model.fit(independent_data, dependent_data)
 
         return TrainedSklearnModel(self, trained_regression)
-
-    @classmethod
-    def validity_check(klass):
-        validity_check = ParametersGeneralValidityCheck()
-        validity_check.set_range_parameter("lasso_regression.alpha", 0.0, 1.0)
-        return validity_check
-
-    def get_alpha(self):
-        return self.parameter_set.get("lasso_regression.alpha")

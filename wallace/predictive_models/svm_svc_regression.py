0575bae90f24765d078124e690469ec6072f5bb2
wangjohn <wangjohn@mit.edu>
2014-05-21 15:39:37 +0000

    Removing unwanted print statements.

--- wallace/predictive_models/svm_svc_regression.py
+++ wallace/predictive_models/svm_svc_regression.py
@@ -11,9 +11,6 @@ class SvmSvcRegression(SklearnModel):
                 )
         independent_data = self.get_independent_variable_data(dataset)
         dependent_data = self.get_dependent_variable_data(dataset)
-        print independent_data
-        print dependent_data
-
         trained_regression = model.fit(independent_data, dependent_data)
 
         return TrainedSklearnModel(self, trained_regression)

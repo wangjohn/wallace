bda500cceccbb5b4bb08eaa276dad184ec45a699
wangjohn <wangjohn@mit.edu>
2014-05-24 17:21:18 +0000

    Working on second (failing) stage handler test.

--- tests/optimization_algorithms/test_optimization_algorithm_stage_handler.py
+++ tests/optimization_algorithms/test_optimization_algorithm_stage_handler.py
@@ -46,3 +46,35 @@ class OptimizationAlgorithmStageHandlerTest(TestCase):
 
         self.assertEqual("before_stage.fake_stage1", fake_stage1_exception.exception.value)
         self.assertEqual("before_stage.fake_stage2", fake_stage2_exception.exception.value)
+
+    def test_stage_handler_for_multi_step_stage_progression_with_holes(self):
+        stage_handler = OptimizationAlgorithmStageHandler(self.settings, {
+                FakeStage1: (0.1, 0.3),
+                FakeStage2: (0.4, 0.5),
+                FakeStage1: (0.5, 0.6)
+            })
+
+        stage_handler.run_stage(0, total_steps=10)
+
+        with self.assertRaises(FakeException) as exception1:
+            stage_handler.run_stage(1, total_steps=10)
+
+        with self.assertRaises(FakeException) as exception2:
+            stage_handler.run_stage(2, total_steps=10)
+
+        stage_handler.run_stage(3, total_steps=10)
+        with self.assertRaises(FakeException) as exception4:
+            stage_handler.run_stage(4, total_steps=10)
+
+        with self.assertRaises(FakeException) as exception5:
+            stage_handler.run_stage(5, total_steps=10)
+
+        stage_handler.run_stage(6, total_steps=10)
+        stage_handler.run_stage(7, total_steps=10)
+        stage_handler.run_stage(8, total_steps=10)
+        stage_handler.run_stage(9, total_steps=10)
+
+        self.assertEqual("before_stage.fake_stage1", exception1.exception.value)
+        self.assertEqual("on_step.fake_stage1", exception2.exception.value)
+        self.assertEqual("before_stage.fake_stage2", exception4.exception.value)
+        self.assertEqual("on_step.fake_stage2", exception5.exception.value)
